3b57ace6480095bcd2a3c2cdb7df4a27d58ee789
kinxtar <jacklin0317@outlook.com>
2024-08-30 09:38:54 -0400

    fix: logo

---
+++ Frontend/src/hooks/contracts/useCoverInfo.ts
@@ -0,0 +1,58 @@
+import {useEffect} from 'react';
+
+import { ICoverContract } from "@/constant/contracts";
+
+import {useBlockNumber, useReadContract} from 'wagmi';
+import { ICover, RiskType } from "@/types/main";
+import { bnToNumber } from "@/lib/formulat";
+
+type CoverType = [
+  bigint,
+  string,
+  RiskType,
+  string,
+  bigint,
+  bigint,
+  bigint,
+  bigint,
+  string,
+]
+
+export const useCoverInfo = (coverId: number) => {
+  const {data: blockNumber} = useBlockNumber({watch: true});
+  const {data: userCover, refetch} = useReadContract({
+    abi: ICoverContract.abi,
+    address: ICoverContract.address as `0x${string}`,
+    functionName: 'covers',
+    args: [coverId],
+  })
+
+  console.log('raw cover info:', userCover)
+
+  useEffect(() => {
+    refetch();
+  }, [blockNumber]);
+
+  if (!userCover) return undefined;
+
+  try {
+    const result = userCover as CoverType;
+    if (!result) return undefined;
+
+    return {
+      id: Number(result[0]),
+      coverName: result[1],
+      riskType: result[2],
+      chains: result[3],
+      capacity: Number(result[4]),
+      cost: Number(result[5]),
+      maxAmount: bnToNumber(result[6]),
+      poolId: Number(result[7]),
+      CID: result[8],
+    };
+
+
+  } catch (error) {
+    return undefined;
+  }
+};
--- Frontend/src/screen/purchase/components/myCover.tsx
+++ Frontend/src/screen/purchase/components/myCover.tsx
@@ -5,19 +5,27 @@ import Button from '@/components/button/button';
 
 import { MyCoverType } from '@/screen/purchase/types';
 import { IUserCover } from "@/types/main";
-import { bnToNumber, UNIXToDate } from "@/lib/formulat";
+import { bnToNumber, getRiskTypeName, UNIXToDate } from "@/lib/formulat";
 import { ClaimContext } from "@/contexts/ClaimContext";
+import { useUserCoverInfo } from "@/hooks/contracts/useUserCoverInfo";
+import { useAccount } from "wagmi";
+import { useCoverInfo } from "@/hooks/contracts/useCoverInfo";
 
 export type CoverProps = {
   onSubmit?: () => void;
 } & IUserCover;
 
 export const MyCover = (props: CoverProps): JSX.Element => {
-  const { coverId, coverName, coverValue: coverValueBN, endDay: endTimeStamp } = props;
+  const { coverId, coverName, coverValue: coverValueBN, endDay: endTimeStamp, riskType } = props;
   const endDate = UNIXToDate(endTimeStamp || 0n);
   const router = useRouter();
+  const riskTypeName = getRiskTypeName(riskType);
 
   const coverValue = bnToNumber(coverValueBN || 0n);
+
+  const { address } = useAccount();
+
+  const coverInfo = useCoverInfo(Number(coverId))
 
   const handleLinkDetail = useCallback(() => {
     router.push(`/claim/?coverId=${coverId}`);
@@ -26,12 +34,14 @@ export const MyCover = (props: CoverProps): JSX.Element => {
   return (
     <div className='bg-background-100 flex w-full flex-col gap-5 rounded-[15px] p-5'>
       <div className='flex items-center gap-[10px]'>
-        <div className='h-[60px] w-[60px] rounded-full bg-white' />
+        <div className='h-[60px] w-[60px] rounded-full overflow-hidden'>
+          <img src={coverInfo?.CID} className="w-full h-full" alt='logo' />
+        </div>
         <div className='flex flex-col gap-1'>
           <div className='text-lg font-semibold leading-[22px]'>{coverName}</div>
           <div className='flex items-center gap-1'>
-            <div className='h-5 w-5 rounded-full bg-white' />
-            {/* <div className='text-sm'>{detail}</div> */}
+            {/* <div className='h-5 w-5 rounded-full bg-white' /> */}
+            {riskTypeName && (<div className='text-sm'>{riskTypeName}</div>)}
           </div>
         </div>
       </div>
